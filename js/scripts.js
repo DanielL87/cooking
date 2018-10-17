4094daca4d1b7b61492c8b8b78d041bf0b4741db
Daniel Lira and Gulzat Karimova <gulzat@>
2018-10-17 15:16:24 -0700

    Added user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
 var gallonsToLiters = function(gallons) {
 	return gallons * 3.78541
 }
+var ounceToPounds = function (ounce) {
+	return ounce * 0.0625
+}
 
-var gallons = parseFloat(prompt("Enter number of gallons to convert to liters:"))
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+    var gallons = $("#gallons").val();
+    var result = gallonsToLiters(gallons);
+    $("#output").text(result);
 
-alert(gallonsToLiters(gallons))
+	});
+});
+
+$(document).ready(function() {
+	$("form#formTwo").submit(function(event) {
+		event.preventDefault();
+		var ounce = $("#ounce").val();
+		var result = ounceToPounds(ounce);
+		$("#output2").text(result);
+	});
+});
